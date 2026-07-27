55d0a28650679796cec582704349b83c51e37877
sairajkandkar0110-woapipa <sairajkandkar0110@gmail.com>
2026-07-27 11:26:11 +0530

    Update twrp_beryl.mk

--- twrp_beryl.mk
+++ twrp_beryl.mk
@@ -1,12 +1,16 @@
-# Inherit from Omni/OrangeFox common recovery configurations
+# 1. Inherit core AOSP configurations (REQUIRED for Android 14 to recognize BRAND)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# 2. Inherit from Omni/OrangeFox common recovery configurations
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Device Identification
+# 3. Device Identification
 PRODUCT_NAME := twrp_beryl
 PRODUCT_DEVICE := beryl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO M7 PRO
 
-# Inherit device board configuration
-include $(LOCAL_DIR)/BoardConfig.mk
+# 4. Inherit device board configuration explicitly
+include device/xiaomi/beryl/BoardConfig.mk
