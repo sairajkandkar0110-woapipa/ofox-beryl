f92a30e005fb31a47a36630a5d32416b07470eb8
sairajkandkar0110-woapipa <sairajkandkar0110@gmail.com>
2026-07-27 11:23:18 +0530

    Update twrp_beryl.mk

--- twrp_beryl.mk
+++ twrp_beryl.mk
@@ -6,7 +6,7 @@ PRODUCT_NAME := twrp_beryl
 PRODUCT_DEVICE := beryl
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Xiaomi Beryl
+PRODUCT_MODEL := POCO M7 PRO
 
 # Inherit device board configuration
 include $(LOCAL_DIR)/BoardConfig.mk
