60838e4f0811f816c5d8c9140d30220dffc572bc
sairajkandkar0110-woapipa <sairajkandkar0110@gmail.com>
2026-07-27 10:51:39 +0530

    Update twrp_beryl.mk

--- twrp_beryl.mk
+++ twrp_beryl.mk
@@ -1,1 +1,12 @@
+# Inherit from Omni/OrangeFox common recovery configurations
+$(call inherit-product, vendor/omni/config/common.mk)
 
+# Device Identification
+PRODUCT_NAME := twrp_beryl
+PRODUCT_DEVICE := beryl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Xiaomi Beryl
+
+# Inherit device board configuration
+include $(LOCAL_DIR)/BoardConfig.mk
